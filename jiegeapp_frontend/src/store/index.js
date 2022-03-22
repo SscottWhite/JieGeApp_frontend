1e4d7de491a2b4f032d9cd9bb24901f85246345f
God <2425990981@qq.com>
2022-03-22 10:18:48 +0800

    ++

--- jiegeapp_frontend/src/store/index.js
+++ jiegeapp_frontend/src/store/index.js
@@ -1,21 +1,26 @@
 import { createStore } from 'vuex'
 
-export default createStore({
-  state: {
-    count: 0
-  },
-  getters: { //（可以认为是 store 的计算属性）。
-    doit(state){
-      return state.count
-    }
-  },
-  mutations: {  //methods
-     increment(state){
-        state.count++
-     }
-  },
-  actions: {
-  },
-  modules: {
+
+const state = {
+  count: 0
+}
+const getters = {
+  doit(state){
+    return state.count
   }
+}
+const mutations = {
+  increment(state){
+    state.count++
+  }
+}
+const actions = {}
+const modules = {}
+
+export default createStore({
+    state,
+    getters,
+    mutations,
+    actions,
+    modules
 })
