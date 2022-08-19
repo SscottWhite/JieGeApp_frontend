af6022e6461a5fc3612d4d8adf730b998dc3ce2c
God <2425990981@qq.com>
2022-08-19 16:03:23 +0800

    函数

--- JGReactApp/src/component/header/HeaderContent.js
+++ JGReactApp/src/component/header/HeaderContent.js
@@ -1,11 +1,16 @@
 import React , {Component} from 'react';
 import styles from './HeaderContent.css'
+import Footer from '../footer/Footer';
 
 class HeaderContent extends Component {
 
     render() {
         return (
-            <div className={styles.header}>123123123</div>
+            
+            <div className={styles.header}>
+                123123123
+                <Footer />
+            </div>
         ) 
     }
 }
