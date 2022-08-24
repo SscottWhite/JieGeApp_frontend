dae0d4be86c494c345b56d34dd198df216d2177b
God <2425990981@qq.com>
2022-08-24 16:27:23 +0800

    气泡卡片

---
+++ JGReactApp/src/component/itemPage/Item1.js
@@ -0,0 +1,13 @@
+
+import styles from './item1.css'
+
+function Item1() {
+    return (
+        <div className={styles.demo1}>
+            <a href="" target='_black' className={styles.demo2}>测试1</a>
+            <a href="" target='_black' className={styles.demo2} >测试2</a>
+        </div>
+    )
+}
+
+export default Item1;
--- JGReactApp/src/pages/DashPages/index.js
+++ JGReactApp/src/pages/DashPages/index.js
@@ -2,14 +2,25 @@
 import React, {Component} from 'react';
 import styles from './index.css';
 import classnames from 'classnames';
+import { Popover } from 'antd';
+
+
+import Item1 from '../../component/itemPage/Item1';
 
 class DashPage extends Component {
     state = {
         content : `参看这个${'<a href="https://www.baidu.com">百度<a/>'}`
     }
     render() {
+        const content = (
+            <div>
+              <p>Content</p>
+              <p>Content</p>
+            </div>
+          );
         return (
             <div>
+                
                 <div className={styles.dashHold}></div>
                 <div className={styles.dashHeader}> 
                     <div className='headerFront'>
@@ -21,12 +32,15 @@ class DashPage extends Component {
                         <a href='https://www.baidu.com' className={classnames(styles.demo1,styles.demo2)} target='_black'>视频</a>
                         <a href='https://www.baidu.com' className={classnames(styles.demo1,styles.demo2)} target='_black'>图片</a>
                         <a href='https://www.baidu.com' className={classnames(styles.demo1,styles.demo2)} target='_black'>网盘</a>
-                        <span className={classnames(styles.demo1,styles.demo2,styles.demo3)} data-descr={this.state.content}>更多</span>
+                        {/* <span className={classnames(styles.demo1,styles.demo2,styles.demo3)} data-descr={this.state.content}>更多</span> */}
+                        <Popover content={ <Item1 />} >
+                            <span className={classnames(styles.demo1,styles.demo2)} >更多</span>
+                        </Popover>
                     </div>
                     <div className='headerEnd'>
-
-                    </div>
+                    </div>               
                 </div>
+                {/* <Item1 items={styles.demo3}/> */}
             </div>
         )
     }
