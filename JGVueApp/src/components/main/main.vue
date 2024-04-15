3a4109f7451854a433f3cbf3c05ccfbfdd2125fc
God <2425990981@qq.com>
2024-04-15 16:49:52 +0800

    测试+1

--- JGVueApp/src/components/main/main.vue
+++ JGVueApp/src/components/main/main.vue
@@ -1,18 +1,36 @@
 <!--  -->
 <template>
   <div>
-    {{name}}
+    <span>123</span>
+    <child @newClick2="newClick3" ></child>
+    <input type="text" v-model="msg" />
+    <button @click="unwatch">watch</button>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-        name: ' do you mind if  i smoke'
+  import child from './main2.vue'
+
+  export default{
+    name:'main',
+    data(){
+      msg:""
+    },
+    components:{
+      child
+    },
+    methods:{
+      newClick3(value){
+         console.log("看这:"+value)
+      },
+      unwatch(){
+         this.$watch("msg",function(newvar,oldvar){
+            console.log(newvar,oldvar)
+         })
+      }
     }
   }
-}
+
 </script>
 
 <style  scoped>
---
+++ JGVueApp/src/components/main/main2.vue
@@ -0,0 +1,24 @@
+<!--  -->
+<template>
+  <div>
+    <button @click='newClick'>here</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods:{
+    newClick(){
+           this.$emit('newClick2',88)
+        }
+  }
+}
+</script>
+
+<style  scoped>
+
+</style>
