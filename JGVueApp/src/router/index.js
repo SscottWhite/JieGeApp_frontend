f469228e6756fcbc6aebde291430850c6f8f5d2c
God <2425990981@qq.com>
2024-06-03 11:37:41 +0800

    测试13

--- JGVueApp/src/router/index.js
+++ JGVueApp/src/router/index.js
@@ -4,7 +4,8 @@ import main from '../components/main/main.vue'
 
 const routes = [
   { path: '/login', name: 'login', component: () => import('../components/login/LoginTable.vue') },
-  { path: '/main', name: 'main' , component: main}
+  { path: '/main', name: 'main' , component: main},
+  { path: '/uis', name: 'uis', component: () => import('../components/test/part13_card.vue')}
 ]
 
 const router = createRouter({
